23faa44dd4fe978392b70a3a10f9a21ea09646ef
ID74 <shakirov.vm@phystech.edu>
2022-05-02 17:25:16 +0300

    some better

--- Multithread/integral.c
+++ Multithread/integral.c
@@ -2,21 +2,24 @@
 #define _GNU_SOURCE
 #include <math.h>
 #include <stdio.h>
-#include <time.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <sys/sysinfo.h>
 
 #ifndef THREADS
 #define THREADS 1
 #endif
 
 #define e 2.71828182846
+#define PAGE_SIZE 4096
 
-double sum_local[THREADS];
+double sum_local[THREADS][PAGE_SIZE / sizeof(double)];
 
 struct integral_param {
 
+	pthread_t thread_id;
 	double start;
 	double fin;
 	double delta;
@@ -25,45 +28,34 @@ struct integral_param {
 
 void* integral(void* data) {
 
-	clock_t start_time = 0, fin_time = 0;
-	start_time = clock();
-
-
 	struct integral_param* param = data;
 
-	sum_local[param->num] = 0;
+	cpu_set_t cpuset;
+	CPU_ZERO(&cpuset);
+	CPU_SET(param->num * 2, &cpuset);
 
-	for( ; param->start < param->fin; param->start += param->delta) {
-		double x = param->start + param->delta / 2;
-
-		sum_local[param->num] += pow(e, x) * param->delta;
+	int err = pthread_setaffinity_np(param->thread_id, sizeof(cpuset), &cpuset);
+	if (err != 0) {
+		printf("Err is %d in %d\n", err, param->num);
+	   perror("CPU_ALLOC");
+	    //exit(EXIT_FAILURE);
 	}
 
+	sum_local[param->num][0] = 0;
 
- 	fin_time = clock();
+	for( ; param->start < param->fin; param->start += param->delta) {
 
-	size_t time = (fin_time - start_time) / (CLOCKS_PER_SEC / 1000);
+		double x = param->start + param->delta / 2;
+		sum_local[param->num][0] += (pow(e, x) + pow(e, -x)) * param->delta;
+	}
 
-	printf("time[%u] - %ld mc\n", param->num, time);
+	//CPU_FREE(&cpuset);
 }
 
 int main(int argc, char** argv) {
 
-	cpu_set_t *cpusetp[THREADS];
-	size_t size[THREADS];
-	int num_cpus = 1;
+	printf("You have %ld proc - %d\n", sysconf(_SC_NPROCESSORS_ONLN), get_nprocs());
 
-	for(int i = 0; i < THREADS; i++) {
-		cpusetp[i] = CPU_ALLOC(1);
-		if (cpusetp == NULL) {
-		   perror("CPU_ALLOC");
-		   exit(EXIT_FAILURE);
-		}
-
-		size[i] = CPU_ALLOC_SIZE(num_cpus);
-	
-		CPU_ZERO_S(size[i], cpusetp[i]);
-	}
 /*
 	if (argc < 2) {
 	   fprintf(stderr, "Usage: %s <num-cpus>\n", argv[0]);
@@ -71,18 +63,7 @@ int main(int argc, char** argv) {
 	}
 
 	num_cpus = atoi(argv[1]);
-*/
-
-	for (int cpu = 0; cpu < num_cpus; cpu += 2) {//Hyperthreading
-
-		CPU_SET_S(cpu, size[cpu], cpusetp[cpu]);
-		printf("CPU_COUNT() [%d] of set:    %d\n", cpu, CPU_COUNT_S(size[cpu], cpusetp[cpu]));
-	}
-
-	
-
-   //    int pthread_setaffinity_np(pthread_t thread, size_t cpusetsize,
-    ///                              const cpu_set_t *cpuset);
+*/	
 
 	printf("Threads - %d\n", THREADS);
 
@@ -100,6 +81,7 @@ int main(int argc, char** argv) {
 
 	for (int i = 0; i < THREADS; i++) {
 
+		// params[i].thread_id ?
 		params[i].start = global_start + i * interval;
 		params[i].fin = global_start + (i + 1) * interval;
 		params[i].delta = global_delta;
@@ -107,36 +89,17 @@ int main(int argc, char** argv) {
 		pthread_attr_init(&thread_attrs[i]);
 	}
 
-	clock_t start_time = 0, fin_time = 0;
 	double sum_global = 0;
 
-	start_time = clock();
 	for (int i = 0; i < THREADS; i++) {
 
-		err = pthread_create(&thread_ids[i], NULL, integral, &params[i]);
-
-		err = pthread_setaffinity_np(thread_ids[i], size[i], cpusetp[i]);
-		if (err != 0) {
-		    perror("CPU_ALLOC");
-		    //exit(EXIT_FAILURE);
-		}
-
+		err = pthread_create(&(params[i].thread_id), NULL, integral, &params[i]);
 	}
 	for (int i = 0; i < THREADS; i++) {
 
-		err = pthread_join(thread_ids[i], NULL);
+		err = pthread_join(params[i].thread_id, NULL);
+		sum_global += sum_local[i][0];
 	}
-	for (int i = 0; i < THREADS; i++) {
-
-		sum_global += sum_local[i];
-	}
- 	fin_time = clock();
-
-	size_t time = (fin_time - start_time) / (CLOCKS_PER_SEC / 1000);
 
 	printf("integral - %lf\n", sum_global);
-	printf("time - %lu mc\n", time);
-
-//	CPU_FREE(cpusetp);
-
 }
